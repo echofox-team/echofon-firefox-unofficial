5766eb0c0dfabef356b96feccd8ad5be432c1470
Alexis THOMAS <fr.alexisthomas@gmail.com>
2015-03-09 00:53:41 +0100

    Rework build script to keep sources intact

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-cd chrome/Echofon
+
+versionNumber=$(cat VERSION)
+
+rm -rf build
+mkdir -p build/src
+cp -r --parents chrome/ components/ defaults/ modules/ platform/ chrome.manifest install.rdf build/src
+
+cd build/src/chrome/Echofon
 zip -r -9 ../Echofon.jar *
 cd ..
 rm -rf Echofon
+
 cd ..
-zip -r -9 echofon_for_twitter-2.5.3.xpi *
+zip -r -9 ../echofon_for_twitter-$versionNumber.xpi *
